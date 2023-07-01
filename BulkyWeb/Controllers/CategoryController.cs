c24ee01fb2a6ab8073b0af2ae7fe1ccd0c6e232b
Natanael <natanaelsantosbr@gmail.com>
2023-07-01 02:05:04 -0300

    Category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {            
+            var objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Category());
         }
     }
 }
