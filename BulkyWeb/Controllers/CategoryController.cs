35bc9cf48ff6f1b0dd7c0aa8626f710b5458b8c4
Natanael <natanaelsantosbr@gmail.com>
2023-07-02 02:36:34 -0300

    update

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -43,5 +43,32 @@ namespace BulkyWeb.Controllers
             return View();
             
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+
+        }
     }
 }
