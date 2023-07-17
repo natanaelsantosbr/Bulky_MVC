1c0c6811acb549e98e9b049cf7d4da2e23c8bc5b
Natanael <natanaelsantosbr@gmail.com>
2023-07-16 21:04:34 -0300

    fk company user

--- Bulky.Models/Models/ApplicationUser.cs
+++ Bulky.Models/Models/ApplicationUser.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +23,10 @@ namespace Bulky.Models.Models
 
         public string? PostalCode { get; set; }
 
-        ///
+        public int? CompanyId { get; set; }
+
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company Company { get; set; }
     }
 }
